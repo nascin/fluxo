75ff5e357c814c1ea1356406c12bbed27593df3b
Mailson Náscin <48425006+nascin@users.noreply.github.com>
2023-12-20 00:31:13 -0300

    #18 #20 #22 (#23)

--- fluxo/fluxo_server/screens/home/flow.py
+++ fluxo/fluxo_server/screens/home/flow.py
@@ -8,18 +8,17 @@ from fluxo.fluxo_server.screens.home.status_execution import StatusExecution
 
 
 class Flow(ft.UserControl):
-    def __init__(self, flow: ModelFlow):
+    def __init__(self, flow: ModelFlow, flows_executor: FlowsExecutor):
         super().__init__()
         self.flow = flow
+        self.flows_executor = flows_executor
 
     def build(self):
         self.row_flow = ft.Ref[ft.Row]()
         self.text_name = ft.Ref[ft.Text]()
         self.text_interval = ft.Ref[ft.Text]()
 
-        self.container_status = ft.Ref[ft.Container]()
         self.row_executions = ft.Ref[ft.Row]()
-        self.text_status = ft.Ref[ft.Text]()
         self.switch_running = ft.Ref[ft.Switch]()
 
         return ft.Container(
@@ -144,15 +143,14 @@ class Flow(ft.UserControl):
         await self.update_async()
 
     async def on_change_switch_running(self, e):
-        flows_executor = FlowsExecutor()
         flow = ModelFlow.get_by_id(self.flow.id)
 
-        if flow.running:
-            flows_executor.stop_flow_execution([flow])
+        if flow.running: # Stop Flow
+            self.flows_executor.stop_flow_execution([flow])
             e.control.value = False
             e.control.label = 'OFF'
-        else:
-            flows_executor.execute_parallel_flows([flow])
+        else: # Start Flow
+            self.flows_executor.execute_parallel_flows([flow])
             e.control.value = True
             e.control.label = 'ON'
         await self.update_async()
--- fluxo/fluxo_server/screens/home/home.py
+++ fluxo/fluxo_server/screens/home/home.py
@@ -4,6 +4,7 @@ from fluxo.settings import AppThemeColors
 from fluxo.fluxo_server.screens.home.flow import Flow
 from fluxo.fluxo_server.screens.app_bar import AppBar
 from fluxo.fluxo_server.screens.footer import Footer
+from fluxo.fluxo_core.flows_executor import FlowsExecutor
 from fluxo.fluxo_core.database.flow import ModelFlow
 from fluxo.fluxo_core.database.app import ModelApp
 
@@ -11,11 +12,11 @@ from fluxo.fluxo_core.database.app import ModelApp
 class Home(ft.UserControl):
     def __init__(self):
         super().__init__()
+        self.flows_executor = FlowsExecutor()
 
     def build(self):
         self.column_flows = ft.Ref[ft.Column]()
         self.responsiverow_flows = ft.Ref[ft.ResponsiveRow]()
-        self.text_status_schedule = ft.Ref[ft.Column]()
 
         return ft.Column(
             ref=self.column_flows,
@@ -32,23 +33,28 @@ class Home(ft.UserControl):
                                 expand=True
                             ), # Text
                             ft.Tooltip(
-                                message='Synchronize',
-                                content=ft.IconButton(
-                                    icon=ft.icons.SYNC,
-                                    icon_color=AppThemeColors.PRIMARY,
-                                    on_click=self.on_click_iconbutton_sync
+                                message='Update new Flow',
+                                content=ft.FloatingActionButton(
+                                    content=ft.Text('Update new Flow', weight=ft.FontWeight.BOLD),
+                                    bgcolor=AppThemeColors.PRIMARY,
+                                    width=140,
+                                    height=27,
+                                    on_click=self.on_click_floatingactionbutton_update_new_flow
                                 ),
                                 bgcolor=AppThemeColors.QUARTENARY
                             ),
-                            ft.Container(
-                                content=ft.Text(
-                                    ref=self.text_status_schedule,
-                                    text_align=ft.TextAlign.END,
-                                    size=12
-                                ), # Text
-                                bgcolor=AppThemeColors.GREY,
-                                border_radius=ft.border_radius.all(10),
-                                padding=ft.padding.only(left=5, top=3, right=5, bottom=3)
+                            ft.Tooltip(
+                                message='Synchronize',
+                                content=ft.FloatingActionButton(
+                                    content=ft.Icon(
+                                        name=ft.icons.SYNC
+                                    ),
+                                    bgcolor=AppThemeColors.PRIMARY,
+                                    width=50,
+                                    height=27,
+                                    on_click=self.on_click_floatingactionbutton_sync
+                                ),
+                                bgcolor=AppThemeColors.QUARTENARY
                             ),
                         ]
                     ),
@@ -76,40 +82,22 @@ class Home(ft.UserControl):
         if flows:
             for flow in flows:
                 flow = ModelFlow.get_by_name(flow.name)
-                self.responsiverow_flows.current.controls.append(Flow(flow=flow))
+                self.responsiverow_flows.current.controls.append(Flow(flow=flow, flows_executor=self.flows_executor))
                 await self.update_async()
 
-    async def _status_schedule(self):
-        app = ModelApp.get()
-        if app:
-            if app.active:
-                self.text_status_schedule.current.value = 'Schedule ON'
-                self.text_status_schedule.current.color = AppThemeColors.GREEN
-            else:
-                self.text_status_schedule.current.value = 'Schedule OFF'
-                self.text_status_schedule.current.color = AppThemeColors.RED
+    async def on_click_floatingactionbutton_update_new_flow(self, e):
+        self.flows_executor.update_new_flow_in_python_files()
         await self.update_async()
 
-    async def on_click_iconbutton_sync(self, e):
-        flows = ModelFlow.get_all()
-        running = []
-        for flow in flows:
-            running.append(flow.running)
-        if not True in running:
-            app = ModelApp.get()
-            app.active = False
-            app.update(app.id, False)
-
+    async def on_click_floatingactionbutton_sync(self, e):
         await self.clean_async()
         await self.did_mount_async()
 
     async def did_mount_async(self):
         self.task_load_flows = asyncio.create_task(self._load_flows())
-        self.task_status_schedule = asyncio.create_task(self._status_schedule())
 
     async def will_unmount_async(self):
         self.task_load_flows.cancel()
-        self.task_status_schedule.cancel()
 
 
 def view_home():
