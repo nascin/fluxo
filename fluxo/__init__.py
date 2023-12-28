494de69c56c71c71c6f5798d15a1990bb4b3f085
Mailson Náscin <mailson.nascin@gmail.com>
2023-12-27 22:02:15 -0300

    Version dinamic

--- fluxo/__init__.py
+++ fluxo/__init__.py
@@ -1,2 +1,6 @@
+from fluxo.settings import AppSettings
 from fluxo.fluxo_core.flow import Flow
 from fluxo.fluxo_core.task import Task
+
+
+__version__ = AppSettings.VERSION
--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
+from fluxo import __version__
+
 
 setup(
     name='fluxo',
-    version='0.13.4',
+    version=__version__,
+    description='Simple data flow with execution in separate threads and easy scheduling configuration.',
+    maintainer='Mailson Náscin',
+    maintainer_email='mailson.nascin@gmail.com',
     packages=find_packages(),
     package_data={
         '': ['fluxo_server/assets/**/*'],
